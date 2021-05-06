f8c1057137db5b83f1f2be84f95f89e7731f48fa
Vladimir Hambardzumyan <vladimirhambardzumyan24@gmail.com>
2021-05-06 15:01:32 +0400

    Arrays unique or no

--- unicues.js
+++ unicues.js
@@ -1,13 +1,15 @@
 const uniques = (arr) => {
   let array = [];
-  let boolItem = true;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] === arr[j]) {
-        return false;
+  return function () {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] === arr[j]) {
+          return false;
+        }
       }
     }
-  }
-  return true;
+    return true;
+  };
 };
-console.log(uniques([1, 2, 3, 2]));
+const f1 = uniques([3, 4, 2, 3]);
+console.log(f1());
